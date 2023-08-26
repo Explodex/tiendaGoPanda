2bfcfa1c5a7bf3785ccca6372380e9cd60f0b4c5
brayan <brayan.graaf@gmail.com>
2023-08-26 18:20:14 -0500

    añadir los productos al carrito de compras y mostrarlos en la vista carrito

--- src/main/java/app/idat/edu/pe/model/DetalleOrden.java
+++ src/main/java/app/idat/edu/pe/model/DetalleOrden.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,90 +13,76 @@ public class DetalleOrden {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private String numero;
+	private String nombre;
 	private double cantidad;
 	private double precio;
 	private double total;
 	
-	@OneToOne
+	@ManyToOne
 	private Orden orden;
+	
 	@ManyToOne
 	private Producto producto;
 	
 	public DetalleOrden() {
+	
 	}
-
-	public DetalleOrden(Integer id, String numero, double cantidad, double precio, double total) {
+	public DetalleOrden(Integer id, String nombre, double cantidad, double precio, double total) {
 		super();
 		this.id = id;
-		this.numero = numero;
+		this.nombre = nombre;
 		this.cantidad = cantidad;
 		this.precio = precio;
 		this.total = total;
 	}
-
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
-	public String getNumero() {
-		return numero;
+	public String getNombre() {
+		return nombre;
 	}
-
-	public void setNumero(String numero) {
-		this.numero = numero;
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
-
 	public double getCantidad() {
 		return cantidad;
 	}
-
 	public void setCantidad(double cantidad) {
 		this.cantidad = cantidad;
 	}
-
 	public double getPrecio() {
 		return precio;
 	}
-
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
-
 	public double getTotal() {
 		return total;
 	}
-
 	public void setTotal(double total) {
 		this.total = total;
 	}
-
+	
+	
 	public Orden getOrden() {
 		return orden;
 	}
-
 	public void setOrden(Orden orden) {
 		this.orden = orden;
 	}
-
 	public Producto getProducto() {
 		return producto;
 	}
-
 	public void setProducto(Producto producto) {
 		this.producto = producto;
 	}
-
 	@Override
 	public String toString() {
-		return "DetalleOrden [id=" + id + ", numero=" + numero + ", cantidad=" + cantidad + ", precio=" + precio
+		return "DetalleOrden [id=" + id + ", nombre=" + nombre + ", cantidad=" + cantidad + ", precio=" + precio
 				+ ", total=" + total + "]";
 	}
-	
-	
 
 }
