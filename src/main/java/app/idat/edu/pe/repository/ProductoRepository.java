11b44703755b783706d3e2b0131c874f0129f1f2
brayan <brayan.graaf@gmail.com>
2023-08-26 02:01:23 -0500

    crear el repository y las clases de servicio y el metodo show de productoController

---
+++ src/main/java/app/idat/edu/pe/repository/ProductoRepository.java
@@ -0,0 +1,11 @@
+package app.idat.edu.pe.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import app.idat.edu.pe.model.Producto;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer>{
+
+}
