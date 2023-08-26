ffe0f55ec14ae22c8c25718650f0c3fdc564ba82
brayan <brayan.graaf@gmail.com>
2023-08-26 13:04:42 -0500

    implementar las funcionalidades de cargar imagen y mostrar imagen para el admin

--- src/main/java/app/idat/edu/pe/controller/AdministradorController.java
+++ src/main/java/app/idat/edu/pe/controller/AdministradorController.java
@@ -1,15 +1,29 @@
 package app.idat.edu.pe.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import app.idat.edu.pe.model.Producto;
+import app.idat.edu.pe.service.ProductoService;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
 	
+	@Autowired
+	private ProductoService productoService;
+	
 	@GetMapping("")
-	public String home() {
+	public String home(Model model) {
+		
+		List<Producto> productos =productoService.findAll();
+		model.addAttribute("productos", productos);
+		
 		return "administrador/home";
 	}
 
