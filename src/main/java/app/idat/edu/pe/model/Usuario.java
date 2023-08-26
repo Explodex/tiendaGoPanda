96446411e33f8821bc8bb82e42c96e78c7a34032
brayan <brayan.graaf@gmail.com>
2023-08-26 01:42:08 -0500

    crear las clase model, mapeo clases a tablas JPA, integrado el diseño y vistas

--- src/main/java/app/idat/edu/pe/model/Usuario.java
+++ src/main/java/app/idat/edu/pe/model/Usuario.java
@@ -1,7 +1,20 @@
 package app.idat.edu.pe.model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
 public class Usuario {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String username;
@@ -10,6 +23,12 @@ public class Usuario {
 	private String telefono;
 	private String tipo;
 	private String password;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Producto> productos;
+	
+	@OneToMany(mappedBy = "usuario")
+	private List<Orden> ordenes;
 	
 	public Usuario(Integer id, String nombre, String username, String email, String direccion, String telefono,
 			String tipo, String password) {
@@ -74,6 +93,14 @@ public class Usuario {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Producto> getProductos() {
+		return productos;
+	}
+
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
 	}
 
 	@Override
