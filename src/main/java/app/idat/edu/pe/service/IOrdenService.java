0d9b69f3479f9f9e8255238116eb54631b8cf935
brayan <brayan.graaf@gmail.com>
2023-08-27 07:28:37 -0500

    metodo para generar el numerode la orden

--- src/main/java/app/idat/edu/pe/service/IOrdenService.java
+++ src/main/java/app/idat/edu/pe/service/IOrdenService.java
@@ -1,8 +1,11 @@
 package app.idat.edu.pe.service;
 
+import java.util.List;
+
 import app.idat.edu.pe.model.Orden;
 
 public interface IOrdenService {
+	List<Orden> findAll();
 	Orden save (Orden orden);
 
 }
