cec2f5cfc4e049d25e25d39808a4bda8aa7ddba8
brayan <brayan.graaf@gmail.com>
2023-08-27 08:56:27 -0500

    obtener usuario login y sesion

--- src/main/java/app/idat/edu/pe/repository/IUsuarioRepository.java
+++ src/main/java/app/idat/edu/pe/repository/IUsuarioRepository.java
@@ -1,5 +1,7 @@
 package app.idat.edu.pe.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import app.idat.edu.pe.model.Usuario;
 
 @Repository
 public interface IUsuarioRepository extends JpaRepository<Usuario, Integer>{
-	//Optional<Usuario> findByEmail(String email);
+	Optional<Usuario> findByEmail(String email);
 }
--- src/main/java/app/idat/edu/pe/service/IUsuarioService.java
+++ src/main/java/app/idat/edu/pe/service/IUsuarioService.java
@@ -7,5 +7,5 @@ import app.idat.edu.pe.model.Usuario;
 public interface IUsuarioService {
 	Optional<Usuario> findById(Integer id);
 	Usuario save(Usuario usuario);
-
+	Optional<Usuario> findByEmail(String email);
 }
