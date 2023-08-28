383f1228783fb98a46b079df2b34a7bcbcf3fe04
brayan <brayan.graaf@gmail.com>
2023-08-27 20:05:46 -0500

    mostrar la vista compras para el user

--- src/main/java/app/idat/edu/pe/controller/UsuarioController.java
+++ src/main/java/app/idat/edu/pe/controller/UsuarioController.java
@@ -1,15 +1,18 @@
 package app.idat.edu.pe.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import app.idat.edu.pe.model.Orden;
 import app.idat.edu.pe.model.Usuario;
 import app.idat.edu.pe.service.IUsuarioService;
 import jakarta.servlet.http.HttpSession;
@@ -62,6 +65,19 @@ public class UsuarioController {
 			}
 			
 			return "redirect:/";
+		}
+		
+		@GetMapping("/compras")
+		public String obtenerCompras(Model model, HttpSession session) {
+			model.addAttribute("sesion", session.getAttribute("idusuario"));
+			
+			Usuario usuario= usuarioService.findById(  Integer.parseInt(session.getAttribute("idusuario").toString()) ).get();
+			//List<Orden> ordenes= ordenService.findByUsuario(usuario);
+			//logger.info("ordenes {}", ordenes);
+			
+			//model.addAttribute("ordenes", ordenes);
+			
+			return "usuario/compras";
 		}
 
 }
